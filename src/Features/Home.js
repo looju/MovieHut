a2c220ef63fec8637c86b122dc369c885ba0020d
looju <omofade2020@gmail.com>
2022-12-26 21:31:05 +0000

    fetching API call

--- src/Features/Home.js
+++ src/Features/Home.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import { View, StyleSheet, Text } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { PopularService } from "../Services/Core/Popular";
 
 export const Home = () => {
+
+const [popular, setPopular]=useState([])
+const [comingSooon, setComingSoon]=useState([])
+const [topRated, setTopRated]=useState([])
+
+
+useEffect(()=>{
+   PopularService()
+   .then((response)=>{
+    console.log(response)
+   })
+   
+})
+
   return (
     <View>
       <Text style={{ alignItems: "center", justifyContent: "center" }}>
