ccf13539610b8d9ce7b9f48054039bbeccfc4022
looju <omofade2020@gmail.com>
2022-12-27 19:37:26 +0000

    Adding linking

--- src/Features/PopularMovieDetails.js
+++ src/Features/PopularMovieDetails.js
@@ -6,58 +6,74 @@ import {
   Text,
   Image,
   TouchableOpacity,
+  Linking
 } from "react-native";
-import { Popular } from "../Services/Core/Popular";
-
-export const PopularMovieDetails = ({route}) => {
-const {data}=route.params
-
-  
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
+export const PopularMovieDetails = ({ route }) => {
+  const { data } = route.params;
 
   return (
-
     <View style={styles.container}>
       <View style={styles.imageView}>
-      <Image resizeMethod="scale" source={{uri:`${data.img}`}} style={styles.imageStyle}/>
+        <Image
+          resizeMethod="scale"
+          source={{ uri: `${data.img}` }}
+          style={styles.imageStyle}
+        />
       </View>
       <View style={styles.detailsView}>
-      <View style={styles.titleView}>
-        <Text style={styles.titleStyle}>{data.title}</Text>
-      </View>
+        <View style={styles.titleAndLinkView}>
+          <View style={styles.titleView}>
+            <Text style={styles.titleStyle}>{data.title}</Text>
+          </View>
+          <View style={styles.linkView}>
+          <TouchableOpacity onPress={()=>{
+            Linking.openURL(`${data.url}`)
+          }}>
+        <MaterialCommunityIcons name="open-in-new" color="#A020F0" size={30}/>
+        </TouchableOpacity>
+          </View>
+        </View>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-container:{
-    flex:1,
-    backgroundColor:"#000"
-},
-imageView:{
-    width:"50%",
-    height:"45%",
-    marginLeft:"25%"
-},
-imageStyle:{
-    width:"100%",
-    height:"100%"
-},
-detailsView:{
-    backgroundColor:"#A020F0",
-    flex:1,
-    borderTopLeftRadius:15,
-    borderTopRightRadius:15,
-},
-titleView:{
-    marginVertical:10,
-    alignItems:"center",
-    justifyContent:"center",
-  
-},
-titleStyle:{
-    color:"#fff"
-}
-});
-
-
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+  imageView: {
+    width: "50%",
+    height: "45%",
+    marginLeft: "25%",
+  },
+  imageStyle: {
+    width: "100%",
+    height: "100%",
+  },
+  detailsView: {
+    backgroundColor: "#fff",
+    flex: 1,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+  },
+  titleAndLinkView: {
+    marginVertical: 10,
+  },
+  titleStyle: {
+    color: "#000",
+    fontSize: 40,
+    fontFamily: "Tangerine_400Regular",
+  },
+  titleView: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  linkView: {
+    bottom:35,
+    alignItems:"flex-end",
+    justifyContent:"flex-end"
+  }
+})
