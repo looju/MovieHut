44b7943d84023976f4cfa0692e8643f5c27d4b95
looju <omofade2020@gmail.com>
2023-01-01 16:55:16 +0000

    Building the avatar screen

---
+++ src/Services/Core/AvatarImages.js
@@ -0,0 +1,52 @@
+export const AvatarImages = [
+  {
+    image:
+      "https://c4.wallpaperflare.com/wallpaper/635/965/145/actor-dwayne-johnson-dwayne-johnson-dwayne-the-rock-johnson-wallpaper-preview.jpg",
+    id: 1,
+  },
+  {
+    image:
+      "https://c4.wallpaperflare.com/wallpaper/792/640/939/denzel-washington-new-photoshoot-wallpaper-preview.jpg",
+    id: 2,
+  },
+  {
+    image:
+      "https://c4.wallpaperflare.com/wallpaper/500/146/346/leonardo-dicaprio-actor-face-beard-wallpaper-preview.jpg",
+    id: 3,
+  },
+  {
+    image:
+      "https://c4.wallpaperflare.com/wallpaper/892/110/435/tom-hanks-actor-face-smile-wallpaper-preview.jpg",
+    id: 4,
+  },
+  {
+    image:
+      "https://c4.wallpaperflare.com/wallpaper/227/876/911/kevin-hart-2015-actor-award-wallpaper-preview.jpg",
+    id: 5,
+  },
+  {
+    image:
+      "https://c4.wallpaperflare.com/wallpaper/992/351/703/face-jennifer-lawrence-actress-celebrity-wallpaper-preview.jpg",
+    id: 6,
+  },
+  {
+    image:
+      "https://c4.wallpaperflare.com/wallpaper/653/423/614/meryl-streep-actress-oscar-celebrity-wallpaper-preview.jpg",
+    id: 7,
+  },
+  {
+    image:
+      "https://c4.wallpaperflare.com/wallpaper/514/423/744/amy-adams-face-eyes-celebrity-wallpaper-preview.jpg",
+    id: 8,
+  },
+  {
+    image:
+      "https://c4.wallpaperflare.com/wallpaper/305/881/91/4k-photo-jennifer-aniston-wallpaper-preview.jpg",
+    id: 9,
+  },
+  {
+    image:
+      "https://c4.wallpaperflare.com/wallpaper/685/113/280/gal-gadot-celebrity-women-portrait-wallpaper-preview.jpg",
+    id: 10,
+  },
+];
--- src/UserSetup/Avatar.js
+++ src/UserSetup/Avatar.js
@@ -1,14 +1,89 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+} from "react-native";
+import { AvatarImages } from "../Services/Core/AvatarImages";
 
-export const Avatar = () => {
-    return (
-        <View>
-            <Text>hiiiiii</Text>
-        </View>
-    );
-}
+export const Avatar = ({navigation}) => {
+  const renderItem = ({ item }) => (
+    <View style={styles.container}>
+      <View style={styles.avatar}>
+        <TouchableOpacity
+        onPress={()=>navigation.navigate("ProfileName",{data:item})}
+        >
+          <Image
+            source={{ uri: `${item.image}` }}
+            style={{
+              height: Dimensions.get("screen").height * 0.2,
+              width: Dimensions.get("screen").width * 0.4,
+              borderRadius: Dimensions.get("screen").height * 0.2,
+            }}
+            resizeMode="cover"
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 
-const styles = StyleSheet.create({})
+  const header = () => (
+    <View>
+      <View style={styles.picture}>
+        <Image source={require("../../assets/display.png")} />
+      </View>
+      <View style={styles.title}>
+        <Text style={styles.titleStyle}>Choose your avatar</Text>
+      </View>
+    </View>
+  );
 
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={AvatarImages}
+        renderItem={renderItem}
+        ListHeaderComponent={header}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+      />
+    </View>
+  );
+};
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+  picture: {
+    top: Dimensions.get("screen").height * 0.05,
+    width: Dimensions.get("screen").width * 0.4,
+    left: Dimensions.get("screen").width * 0.3,
+    height: Dimensions.get("screen").height * 0.2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    marginVertical: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleStyle: {
+    fontSize: 25,
+    fontFamily: "Arizonia_400Regular",
+    color: "#fff",
+  },
+  avatar: {
+    backgroundColor: "#fff",
+    height: Dimensions.get("screen").height * 0.2,
+    width: Dimensions.get("screen").width * 0.4,
+    left: Dimensions.get("screen").width * 0.05,
+    borderRadius: Dimensions.get("screen").height * 0.2,
+    marginBottom: 10,
+  },
+});
