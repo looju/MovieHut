50b79d35685ebcda61d8e5861f3efccd2194004c
looju <omofade2020@gmail.com>
2023-01-11 13:10:33 +0000

    Adding signup auth

--- src/Services/Core/Auth/Auth.js
+++ src/Services/Core/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React, { createContext,useState } from "react";
+import React, { createContext, useState } from "react";
 import { auth } from "./../../Config/Config";
 import {
   signInWithEmailAndPassword,
@@ -10,20 +10,47 @@ import {
 export const Authorization = createContext();
 
 export const AuthorizationProvider = () => {
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState(null);
 
-  const Login = (email, password) => {
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredentials)=>{
-     setUser(userCredentials.user)
-     setIsLoading(false)
-    })
-    .catch((error)=>{
-        setError(error.message)
-        setIsLoading(true)
-    })
+  const Login = (email, password, repeatedPassword) => {
+    if (repeatedPassword !== password) {
+      setIsLoading(true);
+      setError("Looks like the passwords do not match");
+    } else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredentials) => {
+          setUser(userCredentials.user);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          setError(error.message);
+          console.log(error.code);
+          setIsLoading(true);
+        });
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setUser(user.uid);
+        } else {
+          setIsLoading(true);
+          setUser(null);
+        }
+      });
+    }
+  };
+
+  const signUp = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredentials) => {
+        setUser(userCredentials.user);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        console.log(error.code);
+        setIsLoading(true);
+      });
   };
 };
